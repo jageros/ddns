7e99eccc28a3ece8c82cdea929a6d2bd0a5615dd
Jay Leung <lhj168os@gmail.com>
2020-04-19 15:03:00 +0800

    m start script

--- tools/start.sh
+++ tools/start.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 mkdir -p logs
-touch logs/server.log
+touch logs/ddns.log
 mkdir -p pids
 #nohup ./build/center --id 1 2 >> logs/kingwar.slog & echo $! > pids/center1.pid
 nohup ./build/cmd >> logs/ddns.log & echo $! > pids/cmd.pid
